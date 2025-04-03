b5b7ebd4a8d1b9555c9b65331127b4981347f21e
Гималдинов Айрат Ильшатович <152310@mck-ktits.ru>
2025-04-03 09:53:36 +0300

    s

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,6 @@
+import s from './Search.module.css'
+export default function Search({handleChange}){
+    return(
+        <input onChange={handleChange} type="text" placeholder="Поиск..." className={s.search} />
+    )
+}
--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -2,14 +2,25 @@ import Card from "./Card/Card"
 import CatalogBtn from "./CatalogBtn/CatalogBtn"
 import s from './CatalogPage.module.css'
 import { products } from "../../products"
+import { useState } from "react"
+import Search from "../../components/Search/Search"
 
 export default function CatalogPage(){
+    const[search, setSearch] = useState('');
+    function handleChange(e){
+        setSearch(e.target.value);
+    }
+    const filteredProducts = products.filter(product => product.name.toLowerCase().includes(search.toLowerCase()));
+
+
+
     return(
         <>
             <section className={s.catalog}>
                 <div className="container">
                     <div className={s.catalog_inner}>
                         <h2 className={s.title}>Каталог товаров</h2>
+                        <Search handleChange={handleChange}/>
                         <div className={s.nav}>
                             <p className={s.text}>Категории:</p>
                             <CatalogBtn text="Все товары"/>
@@ -19,7 +30,12 @@ export default function CatalogPage(){
                         </div>
                         <div className={s.cards}>
                             {
-                                products.map(product=><Card {...product}/>)
+                                filteredProducts.length
+                                ?
+                                filteredProducts.map(product=><Card{...product}/>)
+                                :
+                                <p>По запросу "{search}" ничего не найдено</p>
+
                             }
                         </div>
                     </div>
