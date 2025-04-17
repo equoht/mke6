6248d20abaf1c5eef69e8e16fc60843a0f988ed3
Гималдинов Айрат Ильшатович <152310@mck-ktits.ru>
2025-04-17 09:15:59 +0300

    1

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -7,11 +7,29 @@ import Search from "../../components/Search/Search"
 
 export default function CatalogPage(){
     const[search, setSearch] = useState('');
+    const[sorting, setSorting] = useState(0);
+    const[category, setCategory] = useState('all');
+
+
+    function sort(e){
+        setSorting(e.target.value);
+    }
+
     function handleChange(e){
         setSearch(e.target.value);
     }
-    const filteredProducts = products.filter(product => product.name.toLowerCase().includes(search.toLowerCase()));
 
+    function sortProducts(sorting, products){
+        switch(sorting){
+            case 'asc':return [...products].sort((a,b) => a.price - b.price);
+            case 'desc':return [...products].sort((a,b) => b.price - a.price);
+            default: return products;
+
+        }
+    }
+
+    const filteredProducts = products.filter(product => product.name.toLowerCase().includes(search.toLowerCase())&&(product.categoryID == category || category=='all'));
+    const sortAndFilteredProducts = sortProducts(sorting, filteredProducts);
 
 
     return(
@@ -21,23 +39,35 @@ export default function CatalogPage(){
                     <div className={s.catalog_inner}>
                         <h2 className={s.title}>Каталог товаров</h2>
                         <Search handleChange={handleChange}/>
+                        
+                        <select value={sorting} onChange={sort}>
+                            <option value= "0" >Сортировать</option>
+                            <option value= "asc" >По возростанию цены</option>
+                            <option value= "desc" >По убыванию цены</option>
+
+                        </select>
+
+                      
+                        
                         <div className={s.nav}>
                             <p className={s.text}>Категории:</p>
-                            <CatalogBtn text="Все товары"/>
-                            <CatalogBtn text="Шины/колеса"/>
-                            <CatalogBtn text="Масла"/>
-                            <CatalogBtn text="Ароматизаторы"/>
+                            <button onClick={()=> setCategory('all')}><CatalogBtn text="Все товары"/></button>
+                            <button onClick={()=> setCategory(0)}><CatalogBtn text="Шины/колеса"/></button>
+                            <button onClick={()=> setCategory(1)}><CatalogBtn text="Масла"/></button>
+                            <button onClick={()=> setCategory(2)}><CatalogBtn text="Ароматизаторы"/></button>
                         </div>
+
                         <div className={s.cards}>
                             {
-                                filteredProducts.length
+                                sortAndFilteredProducts.length
                                 ?
-                                filteredProducts.map(product=><Card{...product}/>)
+                                sortAndFilteredProducts.map(product=><Card{...product}/>)
                                 :
                                 <p>По запросу "{search}" ничего не найдено</p>
 
                             }
                         </div>
+
                     </div>
                 </div>
             </section>
--- src/products.js
+++ src/products.js
@@ -4,35 +4,46 @@ export const products = [
         name: 'Ароматизатор для автомобиля Diamond candle c ароматом "Кожа и древесина"',
         price: 335,
         imagePath: 'public/card_img.png',
+        categoryID: 0,
     },
     {
         id: 1,
         name: 'Держатель для телефона автомобильный телескопический раздвижной',
         price: 444,
         imagePath: 'public/card_img1.png',
+        categoryID: 1,
+
     },
     {
         id: 2,
         name: 'Ароматизатор для автомобиля Diamond candle c ароматом "Кожа и древесина"',
         price: 335,
         imagePath: 'public/card_img.png',
+        categoryID: 0,
+
     },
     {
         id: 3,
         name: 'Держатель для телефона автомобильный телескопический раздвижной',
         price: 444,
         imagePath: 'public/card_img1.png',
+        categoryID: 1,
+
     },
     {
         id: 4,
         name: 'Ароматизатор для автомобиля Diamond candle c ароматом "Кожа и древесина"',
         price: 335,
         imagePath: 'public/card_img.png',
+        categoryID: 2,
+
     },
     {
         id: 5,
         name: 'Держатель для телефона автомобильный телескопический раздвижной',
         price: 444,
         imagePath: 'public/card_img1.png',
+        categoryID: 0,
+
     },
 ]
